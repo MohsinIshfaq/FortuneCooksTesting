c421a68d5ca9aac5e6d556e9ea6a602339c0a071
mustafashaheen1 <mustafashaheen61@gmail.com>
2024-06-24 09:04:28 +0500

    changes

--- Resturants/Screens/View/Main/ProfileTab/ResonOfBugVC.swift
+++ Resturants/Screens/View/Main/ProfileTab/ResonOfBugVC.swift
@@ -10,7 +10,10 @@ import UIKit
 class ResonOfBugVC: UIViewController {
 
     @IBOutlet weak var lblHeader: UILabel!
+    @IBOutlet weak var txtViewBio       : UITextView!
     
+    let placeholder                        = "Enter Here..."
+    let placeholderColor                   = UIColor.lightGray
     var tag = 0
     
     override func viewDidLoad() {
@@ -34,10 +37,34 @@ extension ResonOfBugVC {
             self.navigationItem.title = "Suggestions"
             lblHeader.text = "How can we make fortune cooks better."
         }
+        removeNavBackbuttonTitle()
+        setupPlaceholder()
     }
     
     func onAppear() {
         hidesBottomBarWhenPushed = false
     }
+    
+    func setupPlaceholder() {
+        txtViewBio.text      = placeholder
+        txtViewBio.textColor = placeholderColor
+    }
 }
 
+
+// MARK: - UITextViewDelegate {}
+extension ResonOfBugVC : UITextViewDelegate{
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.textColor == placeholderColor {
+            textView.text      = nil
+            textView.textColor = UIColor.white
+        }
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text.isEmpty {
+            setupPlaceholder()
+        }
+    }
+}
